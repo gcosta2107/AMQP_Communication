bb8c5215ffa9262c36fd064e13b2adeff8fcb813
Dcmaran <diogocmr2002@gmail.com>
2023-10-29 22:53:17 -0300

    add python receive and send

---
+++ Python/Receiver.py
@@ -0,0 +1,26 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    host='jackal-01.rmq.cloudamqp.com',
+    virtual_host='ziupzwyk',
+    credentials=pika.PlainCredentials('ziupzwyk', 'OJCI06IEsSFy84nSkDGGGsC_FNyeI3Mz')
+))
+
+channel = connection.channel()
+
+exchange_name = 'direct-exchange'
+routing_key = 'rota-um'
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=routing_key)
+
+def callback(ch, method, properties, body):
+    print(f" [x] Recebido: {body.decode()}")
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+print('Aguardando mensagens. Pressione CTRL+C para sair.')
+
+channel.start_consuming()
---
+++ Python/Send.py
@@ -0,0 +1,39 @@
+import pika
+
+def enviar_mensagem(mensagem, channel, routing_key):
+    channel.basic_publish(
+        exchange=directExchangeName,
+        routing_key=routing_key,
+        body=mensagem
+    )
+    print(f"Mensagem enviada: {mensagem}")
+
+def main():
+    try:
+        connection = pika.BlockingConnection(
+            pika.URLParameters("amqps://ziupzwyk:OJCI06IEsSFy84nSkDGGGsC_FNyeI3Mz@jackal-01.rmq.cloudamqp.com/ziupzwyk")
+        )
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange=directExchangeName, exchange_type="direct", auto_delete=True)
+
+
+        while True:
+            mensagem = input("Digite a mensagem (Ctrl+C para encerrar): ")
+            enviar_mensagem(mensagem, channel, routingKey)
+
+    except KeyboardInterrupt:
+        print("Programa encerrado.")
+    except Exception as e:
+        print(f"Erro: {e}")
+    finally:
+        connection.close()
+
+if __name__ == "__main__":
+    directExchangeName = "direct-exchange"
+    routingKey = "rota-um"
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
